b07deeed0c96305a9b9cf931ab521c20a5da1e60
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-04-24 12:05:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7074140317:AAErXcg8AiM-o3cJuZcqkBZd8NwgwBmz8WM")
-API_ID = int(os.environ.get("API_ID", "11973721"))
-API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
-OWNER = int(os.environ.get("OWNER", "1391556668"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "Rename_4GB_Star_Bot")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6245243701:AAFhma8aRZsXGbvUeWKUhcILSuHynCOod_E")
+API_ID = int(os.environ.get("API_ID", "23926873"))
+API_HASH = os.environ.get("API_HASH", "5e258639c6553fe80000c4c9b87742f2")
+OWNER = int(os.environ.get("OWNER", "6012920664"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Anime_Rename_Official_Bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "Star_Bots_Tamil")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Bucket_Bots")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://AnimeComplexOfficial:AnimeComplexOfficial@cluster0.p96sfzd.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "BucketBots")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/9853637eaaf2654ccd503.jpg")
